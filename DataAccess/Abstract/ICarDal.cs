8c32e09651d6800b29e9269c068e378ee63c5e65
büşranur erpay <busranur.erpay@student.iu.edu.tr>
2021-02-20 23:16:37 +0300

    update

--- DataAccess/Abstract/ICarDal.cs
+++ DataAccess/Abstract/ICarDal.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal //Car ın interface i
+    public interface ICarDal:IEntityRepository<Car> //Car ın interface i
     {
-        List<Car> GetAll(); 
+        //List<Car> GetAll(); 
 
-        void Add(Car car);
-        void Update(Car car);
-        void Delete(Car car);
+        //void Add(Car car);
+        //void Update(Car car);
+        //void Delete(Car car);
 
-        List<Car> GetById(int Id);
+        //List<Car> GetById(int Id);
     }
 }
